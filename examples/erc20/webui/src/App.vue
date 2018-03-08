d3057fa42f9832fffbddd6dec6308f05ab383a12
Diego Fernández Barrera <bigomby@gmail.com>
2018-03-08 17:15:20 +0100

    Añadir aplicación web de ejemplo

---
+++ examples/erc20/webui/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+<div id="app">
+  <h1>Token ERC20</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
---
+++ examples/erc20/webui/src/components/Token.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+  <!-- Módulo de información básica del Token -->
+  <h2>Información del Token {{ tokenName }}</h2>
+
+  <p>Total supply: {{ totalSupply }}</p>
+  <p>Precio: {{ price }}</p>
+  <p>Balance: {{ balance }}</p>
+
+  <!-- Módulo de compra -->
+  <h2>Comprar tokens</h2>
+  <div>
+    <input type="text" placeholder="Cantidad"></input>
+  </div>
+  <button>Comprar</button>
+
+  <!-- Módulo para comprobar el saldo de una cuenta -->
+  <h2>Comprobar saldo</h2>
+  <div>
+    <input type="text" placeholder="Dirección"></input>
+  </div>
+  <button>Comprobar</button>
+
+  <!-- Módulo para enviar tokens a otra dirección -->
+  <h2>Enviar tokens</h2>
+  <div class="">
+    <input type="text" placeholder="Dirección"></input>
+  </div>
+  <div>
+    <input type="text" placeholder="Cantidad"></input>
+  </div>
+  <button>Enviar</button>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Token',
+
+  data() {
+    return {
+      totalSupply: 0,
+      price: 0,
+      balance: 0,
+      tokenName: '',
+    };
+  },
+};
+</script>
